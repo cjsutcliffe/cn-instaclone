6ce6f116b630db72854a9d7599f0dc5e6be68f32
Chris Sutcliffe <cjsutcliffe@gmail.com>
2023-01-31 16:07:11 +0000

    cookie updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 import Login from './components/Login';
+import { readCookie } from './common';
+import { authCheck } from './utils/utilities';
 
 
 function App() {
   const [user, setUser] = useState("");
-  const [photos, setPhotos] = useState([]);
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [photos,setPhotos] = useState([]);
+  const [loggedIn,setLoggedIn] = useState(false);
+  const [cookie,setCookie] = useState();
 
-  useEffect(() => {fetchImages()},[]);
+  async function loginWithToken(cookie) {
+    const user = await authCheck(cookie);
+    console.log(user);
+    setUser(user);
+    setCookie(cookie);
+  }
+
+  useEffect(() => {
+    fetchImages();
+    let cookie = readCookie('jwt_token');
+    if (cookie !== false) {
+      loginWithToken(cookie);
+    }
+    },[]);
   
 
-  const myArray = [{name:"Harry"},{name:"George"},{name:"Hermione"},{name:"Mafalda"}];
-  
+
   const fetchImages = async () => {
     const response = await fetch ("https://picsum.photos/v2/list");
-    console.log(response);
+    console.log(response)
     const data = await response.json();
     console.log(data);
     setPhotos(data);
     console.log(photos);
-  };
+    }
 
   return (
     <div className="App">
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import { login } from "../utils";
+import { useState } from "react";
+import { login } from "../utils/utilities";
 
 const Login = ({setter}) => {
     const [username,setUsername] =useState();
@@ -8,7 +8,7 @@ const Login = ({setter}) => {
 
     const submitHandler = async (event) => {
         event.preventDefault();
-        console.log("executing login request");
+        console.log("executing login request")
         await login(username,email,password,setter);
     }
     
@@ -17,7 +17,7 @@ const Login = ({setter}) => {
             <input onChange={(event) => setUsername(event.target.value)} />
             <input onChange={(event) => setEmail(event.target.value)} />
             <input onChange={(event) => setPassword(event.target.value)} />
-            <button onClick={submitHandler()}>Submit</button>
+            <button onClick={submitHandler}>Submit</button>
         </form>
     )
 }
